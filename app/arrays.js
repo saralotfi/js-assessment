b8a0fc43a901426edaaa68b98875f6048e8f45c0
Sara Lotfi <lotfi.sara@gmail.com>
2021-08-02 15:15:08 +0200

    completed array section

--- app/arrays.js
+++ app/arrays.js
@@ -1,59 +1,99 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
-
+  indexOf: function (arr, item) {
+    return arr.indexOf(item);
   },
 
-  sum: function(arr) {
-
+  sum: function (arr) {
+    return arr.reduce((a, b) => a + b, 0);
   },
 
-  remove: function(arr, item) {
-
+  remove: function (arr, item) {
+    return arr.filter((e) => e !== item);
   },
 
-  removeWithoutCopy: function(arr, item) {
+  removeWithoutCopy: function (arr, item) {
+    let length = arr.length;
 
+    for (let i = 0; i < length; i++) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+        i--;
+        length--;
+      }
+    }
+    return arr;
   },
 
-  append: function(arr, item) {
-
+  append: function (arr, item) {
+    arr.push(item);
+    return arr;
   },
 
-  truncate: function(arr) {
-
+  truncate: function (arr) {
+    arr.pop();
+    return arr;
   },
 
-  prepend: function(arr, item) {
-
+  prepend: function (arr, item) {
+    arr.unshift(item);
+    return arr;
   },
 
-  curtail: function(arr) {
-
+  curtail: function (arr) {
+    arr.shift();
+    return arr;
   },
 
-  concat: function(arr1, arr2) {
-
+  concat: function (arr1, arr2) {
+    return arr1.concat(arr2);
   },
 
-  insert: function(arr, item, index) {
-
+  insert: function (arr, item, index) {
+    arr.splice(index, 0, item);
+    return arr;
   },
 
-  count: function(arr, item) {
-
+  count: function (arr, item) {
+    let count = 0;
+    arr.forEach((e) => e === item && count++);
+    return count;
   },
 
-  duplicates: function(arr) {
+  duplicates: function (arr) {
+    let seen = {};
+    const dupes = [];
 
+    for (let i = 0; i < arr.length; i++) {
+      if (seen[arr[i]]) {
+        seen[arr[i]] += 1;
+      } else {
+        seen[arr[i]] = 1;
+      }
+    }
+
+    for (const item in seen) {
+      if (seen[item] > 1) {
+        dupes.push(+item); // create integer of string key name
+      }
+    }
+
+    return dupes;
   },
 
-  square: function(arr) {
-
+  square: function (arr) {
+    arr2 = arr.map((e) => e * e);
+    return arr2;
   },
 
-  findAllOccurrences: function(arr, target) {
-
-  }
+  findAllOccurrences: function (arr, target) {
+    results = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === target) {
+        results.push(i);
+      }
+    }
+    return results;
+  },
 };
